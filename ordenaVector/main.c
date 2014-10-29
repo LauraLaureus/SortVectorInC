94d9c0e3856850ef707789d61bbf570906edb6fa
usuario <usuario@10.22.143.6>
2014-10-29 14:09:11 +0000

    Básico

---
+++ ordenaVector/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: usuario
+ *
+ * Created on 29 de octubre de 2014, 14:06
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
